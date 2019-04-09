69d68a4f385eea6d47409f62580c9d8eb35a1653
BigJunOba <sjy534948129@sina.com>
2019-04-09 20:26:44 +0800

    使用接口和实现来使程序具备更好地扩展性，定义了读取器和写入器，同时使用参数注入的方式进行初始化

--- log_process.go
+++ log_process.go
@@ -6,17 +6,43 @@ import (
 	"time"
 )
 
-type LogProcess struct {
-	rc          chan string // 读取模块 -> 解析模块
-	wc          chan string // 解析模块 -> 写入模块
-	path        string      // 读取文件的路径
-	influxDBDsn string      // influx data source
+// 使用接口做一个抽象，使读取模块实现接口来实现
+type Reader interface {
+	Read(rc chan string)
 }
 
-func (l *LogProcess) ReadFromFile() {
+// 使用接口做一个抽象，使写入模块实现接口来实现
+type Writer interface {
+	Write(rc chan string)
+}
+
+type LogProcess struct {
+	rc    chan string // 读取模块 -> 解析模块
+	wc    chan string // 解析模块 -> 写入模块
+	read  Reader
+	write Writer
+}
+
+// 定义读取文件结构体
+type ReadFromFile struct {
+	path string // 读取文件的路径
+}
+
+// 定义写入文件结构体
+type WriteToInfluxDB struct {
+	influxDBDsn string // influx data source
+}
+
+// ReadFromFile 结构体实现了 Reader 接口
+func (r *ReadFromFile) Read(rc chan string) {
 	// 读取模块
 	line := "message"
-	l.rc <- line
+	rc <- line
+}
+
+func (w *WriteToInfluxDB) Write(wc chan string) {
+	// 写入模块
+	fmt.Println(<-wc)
 }
 
 func (l *LogProcess) Process() {
@@ -25,22 +51,25 @@ func (l *LogProcess) Process() {
 	l.wc <- strings.ToUpper(data)
 }
 
-func (l *LogProcess) WriteToInfluxDB() {
-	// 写入模块
-	fmt.Println(<-l.wc)
-}
-
 func main() {
-	lp := &LogProcess{
-		rc:          make(chan string), // 初始化 channel
-		wc:          make(chan string), // 初始化 channel
-		path:        "/temp/access.log",
+	r := &ReadFromFile{
+		path: "/temp/access.log",
+	}
+
+	w := &WriteToInfluxDB{
 		influxDBDsn: "username&password",
 	}
 
-	go lp.ReadFromFile() // 实际上应该写成这样：(*lp).ReadFromFile() 但是 golang 已经优化了
+	lp := &LogProcess{
+		rc:    make(chan string), // 初始化 channel
+		wc:    make(chan string), // 初始化 channel
+		read:  r,
+		write: w,
+	}
+
+	go lp.read.Read(lp.rc) // 实际上应该写成这样：(*lp).ReadFromFile() 但是 golang 已经优化了
 	go lp.Process()
-	go lp.WriteToInfluxDB()
+	go lp.write.Write(lp.wc)
 
 	time.Sleep(1 * time.Second) // 为了不让创建完协程就退出，使其等待一秒钟保证三个协程执行完毕  打印：MESSAGE
 }
